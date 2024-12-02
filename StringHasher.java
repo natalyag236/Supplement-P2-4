c2ec1c71448094e92d1bb3d4b4b68352675a99f6
Natalya Goldsmith <ngol1755@students.vsu.edu>
2024-12-01 22:47:57 -0500

    doc: add documentation to String Hasher

--- StringHasher.java
+++ StringHasher.java
@@ -1,8 +1,18 @@
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
+/**
+ * Utility class for hashing strings using the SHA-256 algorithm.
+ */
 public class StringHasher {
 
+    /**
+     * Hashes a given input string using the SHA-256 algorithm.
+     *
+     * @param input The input string to be hashed.
+     * @return The hexadecimal representation of the hashed string.
+     * @throws NoSuchAlgorithmException If the SHA-256 algorithm is not available.
+     */
     public static String hashString(String input) throws NoSuchAlgorithmException {
         MessageDigest digest = MessageDigest.getInstance("SHA-256");
         byte[] hashBytes = digest.digest(input.getBytes());
@@ -15,6 +25,11 @@ public class StringHasher {
         return hexResult.toString();
     }
 
+    /**
+     * Main method to demonstrate hashing a sample input string.
+     *
+     * @param args Command line arguments (not used).
+     */
     public static void main(String[] args) {
         try {
             String input = "Fly, Eagles!";
